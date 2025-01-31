68da8e38b90fa285f4d2cb898ac720dd8317a3a7
Gabe Morris <gabemorris1042@gmail.com>
2025-01-31 12:27:18 -0400

    portfolio & services pages modal sizing changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "39b4f4bdd32d0472"; }
+/******/ 		__webpack_require__.h = function() { return "4e7556be38635dc9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
